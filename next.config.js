4de8a1ec512bcb7447f6b21e9f00405210e7406e
Abagna <abagnasalifusasco@gmail.com>
2023-05-15 16:16:23 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,16 +4,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    API: "https://backendapi.gracebusinessservices.co.uk",
-    // API: "http://localhost:8000/api",
-    NEXT_PUBLIC_API: "https://backendapi.gracebusinessservices.co.uk",
+ 
   },
   publicRuntimeConfig: {
-    APP_NAME: "Grace Business Services",
-    DOMAIN: "https://www.gracebusinessservices.co.uk",
-    DEFAULT_URL:
-      "https://res.cloudinary.com/codesmart/image/upload/v1682680587/gracebusiness/default_vzw1ow.png",
-  },
+
+   
   images: {
     unoptimized: true,
   },
